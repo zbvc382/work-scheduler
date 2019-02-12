23e68b34781b9a086d71ab182d9239825aa72f66
Zilvinas Pocius <pocius.zilvinas@gmail.com>
2019-02-12 20:39:06 +0000

    First commit

---
+++ WorkScheduler.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WorkScheduler.API.Models;
+
+namespace WorkScheduler.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
