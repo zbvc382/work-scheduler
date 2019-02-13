4c2a8eaa6663fc7acfe798fd6c752599a3528c53
Zilvinas Pocius <pocius.zilvinas@gmail.com>
2019-02-13 19:54:12 +0000

    Delete values controller and model

--- WorkScheduler.API/Data/DataContext.cs
+++ WorkScheduler.API/Data/DataContext.cs
@@ -6,7 +6,6 @@ namespace WorkScheduler.API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
-
-        public DbSet<Value> Values { get; set; }
+        
     }
 }
