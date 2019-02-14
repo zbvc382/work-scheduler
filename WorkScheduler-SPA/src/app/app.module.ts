f54b31e41b88c95d007fa321738f3e28dde15881
Zilvinas Pocius <pocius.zilvinas@gmail.com>
2019-02-14 02:48:46 +0000

    Remove unused component

--- WorkScheduler-SPA/src/app/app.module.ts
+++ WorkScheduler-SPA/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
    ],
    imports: [
       BrowserModule,
