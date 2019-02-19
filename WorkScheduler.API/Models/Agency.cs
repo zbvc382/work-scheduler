a56dba7be8900bc9bcbd9be15b98397edd1512f2
Zilvinas Pocius <pocius.zilvinas@gmail.com>
2019-02-19 20:51:08 +0000

    Add agency model

---
+++ WorkScheduler.API/Models/Agency.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace WorkScheduler.API.Models
+{
+    public class Agency {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ContactName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string PostCode { get; set; }
+        public ICollection<Tenant> Tenants { get; set; }
+        public ICollection<Job> Jobs { get; set; }
+    }
+}
